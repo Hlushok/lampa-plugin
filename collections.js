4a1481029c67eb46ea20a73b38124b58fc0d2a13
Hlushok <101033368+Hlushok@users.noreply.github.com>
2026-02-01 22:12:41 +0200

    Add sport category to collections

--- collections.js
+++ collections.js
@@ -69,7 +69,14 @@
                    </svg>`,
             url: "discover/tv?language=uk&with_genres=10765",
             source: "tmdb"
-        }
+        },
+        {
+    id: 'sport',
+    title: 'Спорт',
+    icon: `<svg width="24px" height="24px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5-9h10v2H7z"/></svg>`,
+    url: "discover/movie?language=uk&with_keywords=6075",
+    source: "tmdb"
+}
     ];
 
     const menuElements = {};
