5963658e79ee4fb64a2b88d6bbc45b1b102aed9b
Hlushok <101033368+Hlushok@users.noreply.github.com>
2026-02-05 21:37:25 +0200

    Add proxy URL for API requests

--- fx.js
+++ fx.js
@@ -8,6 +8,7 @@
     Lampa.Storage.set('fxapi_uid', unic_id);
   }
   
+  var proxy_url = 'http://cors.byskaz.ru/';
   var api_url = 'http://filmixapp.vip/api/v2/';
   var dev_token = 'user_dev_apk=2.0.1&user_dev_id=' + unic_id + '&user_dev_name=Lampa&user_dev_os=11&user_dev_vendor=FXAPI&user_dev_token=';
   var modalopen = false;
@@ -121,7 +122,7 @@
     };
 
     this.find = function(filmix_id) {
-      var url = api_url;
+      var url = proxy_url + api_url;
 
       end_search(filmix_id);
 
